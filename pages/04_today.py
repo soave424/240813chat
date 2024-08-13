75610e89a5fae483a84810e6925e1801f5300a94
soave424 <89090628+soave424@users.noreply.github.com>
2024-08-13 16:58:49 +0900

    Update 04_today.py

--- pages/04_today.py
+++ pages/04_today.py
@@ -39,11 +39,16 @@ if "messages" not in st.session_state:
 if "store" not in st.session_state:
     st.session_state["store"] = {}
 
-# 모델 선택 메뉴
-selected_model = st.selectbox("LLM 선택", ["gpt-4o", "gpt-4o-mini"], index=0)
+# 사이드바 생성
+with st.sidebar:
+    # 초기화 버튼 생성
+    clear_btn = st.button("대화 초기화")
 
-# 세션 ID를 지정하는 메뉴
-session_id = st.text_input("세션 ID를 입력하세요.", "abc123")
+    # 모델 선택 메뉴
+    selected_model = st.selectbox("LLM 선택", ["gpt-4o", "gpt-4o-mini"], index=0)
+
+    # 세션 ID를 지정하는 메뉴
+    session_id = st.text_input("세션 ID를 입력하세요.", "abc123")
 
 # 화면에 날짜 선택 메뉴 추가
 st.header("날짜 선택")
@@ -72,3 +77,90 @@ if st.button("기념일 확인"):
 def print_messages():
     for chat_message in st.session_state["messages"]:
         st.chat_message(chat_message.role).write(chat_message.content)
+
+# 새로운 메시지를 추가
+def add_message(role, message):
+    st.session_state["messages"].append(ChatMessage(role=role, content=message))
+
+# 세션 ID를 기반으로 세션 기록을 가져오는 함수
+def get_session_history(session_ids):
+    if session_ids not in st.session_state["store"]:  # 세션 ID가 store에 없는 경우
+        # 새로운 ChatMessageHistory 객체를 생성하여 store에 저장
+        st.session_state["store"][session_ids] = ChatMessageHistory()
+    return st.session_state["store"][session_ids]  # 해당 세션 ID에 대한 세션 기록 반환
+
+# 체인 생성
+def create_chain(model_name="gpt-4o"):
+
+    # 프롬프트 정의
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "당신은 Question-Answering 챗봇입니다. 주어진 질문에 대한 답변을 제공해주세요.",
+            ),
+            # 대화기록용 key 인 chat_history 는 가급적 변경 없이 사용하세요!
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "#Question:\n{question}"),  # 사용자 입력을 변수로 사용
+        ]
+    )
+
+    # llm 생성
+    llm = ChatOpenAI(model_name="gpt-4o", openai_api_key=st.session_state.api_key)
+
+    # 일반 Chain 생성
+    chain = prompt | llm | StrOutputParser()
+
+    chain_with_history = RunnableWithMessageHistory(
+        chain,
+        get_session_history,  # 세션 기록을 가져오는 함수
+        input_messages_key="question",  # 사용자의 질문이 템플릿 변수에 들어갈 key
+        history_messages_key="chat_history",  # 기록 메시지의 키
+    )
+    return chain_with_history
+
+# 초기화 버튼이 눌리면...
+if clear_btn:
+    st.session_state["messages"] = []
+
+# 이전 대화 기록 출력
+print_messages()
+
+# 사용자의 입력
+user_input = st.chat_input("궁금한 내용을 물어보세요!")
+
+# 경고 메시지를 띄우기 위한 빈 영역
+warning_msg = st.empty()
+
+if "multiturn_chain" not in st.session_state:
+    st.session_state["multiturn_chain"] = create_chain(model_name=selected_model)
+
+# 만약에 사용자 입력이 들어오면...
+if user_input:
+    chain = st.session_state["multiturn_chain"]
+    if chain is not None:
+        response = chain.stream(
+            # 질문 입력
+            {"question": user_input},
+            # 세션 ID 기준으로 대화를 기록합니다.
+            config={"configurable": {"session_id": session_id}},
+        )
+
+        # 사용자의 입력
+        st.chat_message("user").write(user_input)
+
+        with st.chat_message("assistant"):
+            # 빈 공간(컨테이너)을 만들어서, 여기에 토큰을 스트리밍 출력한다.
+            container = st.empty()
+
+            ai_answer = ""
+            for token in response:
+                ai_answer += token
+                container.markdown(ai_answer)
+
+            # 대화기록을 저장한다.
+            add_message("user", user_input)
+            add_message("assistant", ai_answer)
+    else:
+        # 경고 메시지 출력
+        warning_msg.error("대화 체인이 생성되지 않았습니다.")
